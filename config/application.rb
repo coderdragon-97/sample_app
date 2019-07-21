ab2ff4399a575a7b5862ad6b472c863c9a9dfe58
Vishnu Mohan <vishnu.mohan@freshworks.com>
2019-07-21 22:28:49 +0530

    bug fix in routes.rb

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,4 @@ module SampleApp
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
-  config.assets.initialize_on_precompile = false
 end
